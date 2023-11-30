0e3a3b9eedea2d7cc2362465847293a625f6c177
mandeepsingh04012 <mandeepsingh040121@gmail.com>
2023-11-30 16:33:50 +0530

    Initial commit

---
+++ src/test/java/starter/stepdefinitions/SearchStepDefinitions.java
@@ -0,0 +1,45 @@
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+//import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
+//import net.thucydides.core.annotations.Steps;
+//import org.hamcrest.Matchers;
+
+import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static org.hamcrest.Matchers.contains;
+
+public class SearchStepDefinitions {
+
+
+    @Steps
+    public CarsAPI carsAPI;
+
+    @When("he calls endpoint {string}")
+    public void heCallsEndpoint(String arg0) {
+        SerenityRest.given().get(arg0);
+    }
+
+    @Then("he sees the results displayed for apple")
+    public void heSeesTheResultsDisplayedForApple() {
+        restAssuredThat(response -> response.statusCode(200));
+    }
+
+    @Then("he sees the results displayed for mango")
+    public void heSeesTheResultsDisplayedForMango() {
+//        restAssuredThat(response -> response.body("title", contains("mango")));
+        restAssuredThat(response -> response.statusCode(404));
+    }
+
+//    @Then("he doesn not see the results")
+//    public void he_Doesn_Not_See_The_Results() {
+//        restAssuredThat(response -> response.body("error", contains("True")));
+//    }
+    @Then("he does not see the results")
+    public void hedoesnotseetheresults() {
+//        restAssuredThat(response -> response.body("error", contains("True")));
+        restAssuredThat(response -> response.statusCode(404));
+    }
+}
---
+++ src/test/resources/features/search/post_product.feature
@@ -0,0 +1,14 @@
+@smokeTest
+Feature: Search for the product
+
+### Please use endpoint GET https://waarkoop-server.herokuapp.com/api/v1/search/demo/{product} for getting the products.
+### Available products: "orange", "apple", "pasta", "cola"
+### Prepare Positive and negative scenarios
+
+  Scenario:
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/apple"
+    Then he sees the results displayed for apple
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/mango"
+    Then he sees the results displayed for mango
+    When he calls endpoint "https://waarkoop-server.herokuapp.com/api/v1/search/demo/car"
+    Then he does not see the results
